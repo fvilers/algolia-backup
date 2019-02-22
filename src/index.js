56d10db796da74fb6b8099a09e7b4c9e326356d4
Fabian Vilers <fabian.vilers@dev-one.com>
2019-02-22 13:37:56 +0100

    List indices

--- src/index.js
+++ src/index.js
@@ -8,8 +8,20 @@ if (!isProd()) {
 }
 
 const logger = new Logger();
-const algoliaClient = algoliasearch(
-  process.env.ALGOLIA_APP_ID,
-  process.env.ALGOLIA_ADMIN_KEY
-);
-logger.log('Aloglia client initialized.');
+
+async function main() {
+  // Initialization
+  const algoliaClient = algoliasearch(
+    process.env.ALGOLIA_APP_ID,
+    process.env.ALGOLIA_ADMIN_KEY
+  );
+  logger.log('Aloglia client initialized.');
+
+  // List indices
+  const { items } = await algoliaClient.listIndexes();
+  for (const index of items) {
+    logger.log(index.name);
+  }
+}
+
+main().catch(e => logger.error(e.message));
